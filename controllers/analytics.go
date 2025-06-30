dcd24fdb2a6e70dee526d6bb2a705c368af8f2c1
Harendra Kumar Kanojiya <harendraverma21@gmail.com>
2025-06-30 09:20:37 +0530

    feat: enhance analytics functions to fetch and save data from multiple platforms

--- controllers/analytics.go
+++ controllers/analytics.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/pocketbase/dbx"
@@ -47,6 +49,36 @@ func FetchPostsAnalytics(app *pocketbase.PocketBase) {
 
 }
 
+func SaveUpdateAnalyticsData(app *pocketbase.PocketBase, postId string, data string) {
+
+	analytics := Analytics{}
+	err := app.DB().Select("id").From("analytics").Where(dbx.NewExp("post = {:post}", dbx.Params{"post": postId})).One(&analytics)
+	if err != nil && err.Error() != "sql: no rows in result set" {
+		app.Logger().Error("Error in fetching facebook analytics", "error", err.Error())
+		return
+	}
+	if err != nil && err.Error() == "sql: no rows in result set" {
+		_, err := app.DB().Insert("analytics", dbx.Params{
+			"post":    postId,
+			"data":    data,
+			"created": time.Now(),
+			"updated": time.Now(),
+		}).Execute()
+
+		if err != nil {
+			app.Logger().Error("Error inserting new analytics", "error", err.Error())
+			return
+		}
+
+	} else {
+		record, _ := app.FindRecordById("analytics", analytics.Id)
+		record.Set("data", data)
+		app.Save(record)
+		app.Logger().Info("Successfully fetched the analytics")
+	}
+
+}
+
 func FetchFacebookAnalytics(app *pocketbase.PocketBase, post Post, connection Connection) {
 	url := fmt.Sprintf("https://graph.facebook.com/%s/insights?metric=post_impressions,post_reactions_like_total,post_reactions_love_total,post_reactions_wow_total&access_token=%s", post.PublishedPostId, connection.AccessToken)
 	method := "GET"
@@ -70,31 +102,88 @@ func FetchFacebookAnalytics(app *pocketbase.PocketBase, post Post, connection Co
 		return
 	}
 	data := string(body)
-	analytics := Analytics{}
-	err = app.DB().Select("id").From("analytics").Where(dbx.NewExp("post = {:post}", dbx.Params{"post": post.Id})).One(&analytics)
-	if err != nil && err.Error() != "sql: no rows in result set" {
-		app.Logger().Error("Error in fetching facebook analytics", "error", err.Error())
+	SaveUpdateAnalyticsData(app, post.Id, data)
+
+}
+
+func FetchLinkedInPostAnalytics(app *pocketbase.PocketBase, post Post, connection Connection) {
+	url := fmt.Sprintf("https://api.linkedin.com/v2/socialActions/%s", post.PublishedPostId)
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Authorization", "Bearer "+connection.AccessToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		app.Logger().Error("Error in fetching linkedin analytics", "error", err.Error())
 		return
 	}
-	if err != nil && err.Error() == "sql: no rows in result set" {
+	defer resp.Body.Close()
 
-		_, err := app.DB().Insert("analytics", dbx.Params{
-			"post":    post.Id,
-			"data":    data,
-			"created": time.Now(),
-			"updated": time.Now(),
-		}).Execute()
+	body, _ := io.ReadAll(resp.Body)
+	data := string(body)
+	SaveUpdateAnalyticsData(app, post.Id, data)
+}
 
-		if err != nil {
-			app.Logger().Error("Error inserting new analytics", "error", err.Error())
-			return
-		}
+func FetchInstagramPostAnalytics(app *pocketbase.PocketBase, post Post, connection Connection) {
+	url := fmt.Sprintf("https://graph.facebook.com/v19.0/%s/insights?metric=impressions,reach,engagement,saved&access_token=%s", post.PublishedPostId, connection.AccessToken)
 
-	} else {
-		record, _ := app.FindRecordById("analytics", analytics.Id)
-		record.Set("data", data)
-		app.Save(record)
-		app.Logger().Info("Successfully fetched the facebook analytics")
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		app.Logger().Error("Error in fetching instagram analytics", "error", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	data := string(body)
+	SaveUpdateAnalyticsData(app, post.Id, data)
+}
+
+func FetchPinterestPostAnalytics(app *pocketbase.PocketBase, post Post, connection Connection) {
+	url := fmt.Sprintf("https://api.pinterest.com/v5/pins/%s/analytics", post.PublishedPostId)
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Authorization", "Bearer "+connection.AccessToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		app.Logger().Error("Error in fetching pinterest analytics", "error", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	data := string(body)
+	SaveUpdateAnalyticsData(app, post.Id, data)
+}
+
+func FetchMastodonPostAnalytics(app *pocketbase.PocketBase, post Post, connection Connection) {
+	instanceBaseURL := os.Getenv("MASTODON_BASE_URL")
+
+	var result struct {
+		ID string `json:"id"`
 	}
 
+	err := json.Unmarshal([]byte(post.PublishedPostId), &result)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+
+	url := fmt.Sprintf("%s/api/v1/statuses/%s", instanceBaseURL, result.ID)
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Authorization", "Bearer "+connection.AccessToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		app.Logger().Error("Error in fetching mastodon analytics", "error", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	data := string(body)
+	SaveUpdateAnalyticsData(app, post.Id, data)
 }
